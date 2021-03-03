4ffeca83848d509fc2ed1af42fcff94065b6dba6
xuantruong1999 <kadinxuantruong1999@gmail.com>
2021-03-03 16:33:17 +0700

    fix code show products in page

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,15 +1,39 @@
+import React, {useContext, useState, useEffect} from 'react';
 import Item from './item';
+import AppContext from '../Contexts/AppContext';
+const axios = require('axios').default;
 
 export default function Product()
 {
+    const search = new useContext(AppContext);
+    const [ result, setResult]  = useState([])
+
+   // useEffect(() => alert("begin!"));
+    
+    axios.get('http://localhost:3000/products')
+    .then((response) => {
+        setResult( response.data.filter((product) => {
+        if (search.state.searchString == null){
+            return product;
+        }
+        else (product.name.toUpperCase().includes(search.state.searchString.toUpperCase()) || product.description.toUpperCase().includes(search.state.searchString.toUpperCase()))
+        // eslint-disable-next-line no-lone-blocks
+        {
+            return product;
+        }
+        }))
+    }) 
+    .catch( error => {console.log("ERROR: " + error)});
+
     return(
         <div className="col col-10">
             <div className="row">
-                <Item />
+                {
+                   result.map(product => (
+                        <Item data={product} />
+                    ))
+                }
             </div>
         </div>
     );
 }
-    
-
-    
--- src/Components/item.jsx
+++ src/Components/item.jsx
@@ -1,39 +1,22 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import '../css/product.css';
-import AppContext from '../Contexts/AppContext';
-const axios = require('axios').default;
 
-export default function Item(){
-    const search = new useContext(AppContext);
-    var result = "";
-    axios.get('http://localhost:3004/products')
-        .then((response) => {
-            debugger
-                result = response.data.filter((product) => {
-                if(search.state.searchString == null){
-                    return product
-                }
-                else if(product.name.toUpperCase().includes(search.state.searchString.toUpperCase()) || product.description.toUpperCase().includes(search.state.searchString.toUpperCase())){
-                    return product
-                }
-            }).map((product) => {
-                return (
-                    <div className="wrapper p-2 col-sm-6 col-md-4 col-lg-3" key={product.key}>
-                        <a href={ product.key } className="p-2">
-                            <div className="item">
-                                <img className="card-img-top" src={product.link} alt="Card cap img-responsive" />
-                                <div className="note-like">
-                                    <span className="note-content">Like</span>
-                                </div>
-                                <div className="card-body text-center">
-                                    <p className="card-text">{product.name + " " + product.description }</p>
-                                    <div>{product.price}</div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>                
-                );
-            })
-        })
-    return(result);
+export default function Item(props){
+    debugger
+    return (
+        <div className="wrapper p-2 col-sm-6 col-md-4 col-lg-3" key={props.data.key}>
+            <a href={ props.data.key } className="p-2">
+                <div className="item">
+                    <img className="card-img-top" src={props.data.link} alt="Card cap img-responsive" />
+                    <div className="note-like">
+                        <span className="note-content">Like</span>
+                    </div>
+                    <div className="card-body text-center">
+                        <p className="card-text">{props.data.name + " " + props.data.description }</p>
+                        <div>{props.data.price}</div>
+                    </div>
+                </div>
+            </a>
+        </div>                
+    );
 }
