29913b8dbe7d5883c976d6f611c6a68211610ca2
xuantruong1999 <kadinxuantruong1999@gmail.com>
2021-05-15 22:54:57 +0700

    add logo , carousel done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,35 +1,22 @@
 import React from 'react';
-
+import '../Sass/components/footer.scss';
 class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { date: new Date()}
-    }
-
-    componentDidMount(){
-        this.timeID = setInterval(
-            () => this.tick(), 
-            1000
-            );
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.timeID);
-    }
-
-    tick(){
-        this.setState( 
-            {
-                date: new Date()
-            }
-        );
-    }
 
     render(){
         return(
-            <footer className="page-footer font-small text-center bg-secondary">
-                <h2>Timer</h2>
-                <p>{ this.state.date.toLocaleTimeString() }</p>
+            <footer className="page-footer font-small text-center d-flex" id="footer">
+                <div className="flex-fill p-2">
+                    
+                </div>
+                <div className="flex-fill p-2">
+                    
+                </div>
+                <div className="flex-fill p-2">
+                    abc
+                </div>
+                <div className="flex-fill p-2">
+                    abc
+                </div>
             </footer>
         );
     };
